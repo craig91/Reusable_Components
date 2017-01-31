e0745fdaf3c7370313bc06b9c9c641b28ea65320
craig91 <craigdejean1@gmail.com>
2017-01-30 22:54:16 -0500

    scroll component

---
+++ SideNav-Cover/src/components/scroll.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default React.createClass({
+    render: function() {
+        return (
+            <div>
+              <div className="pic">
+              
+              </div>
+              <div className="pic2">
+              
+              </div>
+              <div className="pic3">
+              
+              </div>
+            
+            </div>
+        
+        )
+    }
+})
